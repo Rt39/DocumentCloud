e92f0366265f356140fac7c4b025dacf02668c47
Rt39 <sunrainshr@qq.com>
2022-05-20 14:31:24 +0800

    完成权限管理功能

---
+++ server/src/main/java/com/example/DocumentCloud/util/StringUtil.java
@@ -0,0 +1,42 @@
+package com.autumn.util;
+
+/**
+ * @program: chapter2->StringUtil
+ * @description: 字符串工具类
+ * @author: qiuyu
+ * @create: 2018-08-15 20:20
+ **/
+public class StringUtil {
+
+    /**
+     * 判断是否为空字符串最优代码
+     * @param str
+     * @return 如果为空，则返回true
+     */
+    public static boolean isEmpty(String str){
+        return str == null || str.trim().length() == 0;
+    }
+
+    /**
+     * 判断字符串是否非空
+     * @param str 如果不为空，则返回true
+     * @return
+     */
+    public static boolean isNotEmpty(String str){
+        return !isEmpty(str);
+    }
+
+    /**
+     * 分隔符
+     */
+    public static final String SEPARATOR = String .valueOf((char)29);
+
+    public static String[] splitString(String str,String spliter){
+        return str.split(spliter);
+    }
+
+    /*测试*/
+    public static void main(String[] args) {
+        System.out.println(SEPARATOR);
+    }
+}
